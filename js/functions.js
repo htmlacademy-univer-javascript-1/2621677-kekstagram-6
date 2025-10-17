1d9a770b04e084f4a080c96c1dd456cf7b46cdeb
Павел <pashenka_tikhonov_98@mail.ru>
2025-10-17 20:23:31 +0500

    module4-task1: исправлены ошибки

--- js/functions.js
+++ js/functions.js
@@ -1,72 +1,30 @@
-// Функция для генерации случайного числа в диапазоне
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-// Генератор уникальных ID для комментариев
-const generateCommentId = () => {
-  let lastId = 100;
-  return () => {
-    lastId += 1;
-    return lastId;
-  };
-};
+const isPalindrome = (string) => {
+  const cleanString = string.replace(/\s/g, '').toLowerCase();
+  let reversedString = '';
 
-const createCommentId = generateCommentId();
-
-// Массив сообщений для комментариев
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-// Массив имен для комментаторов
-const NAMES = [
-  'Артём', 'Мария', 'Александр', 'Елена', 'Дмитрий',
-  'Ольга', 'Сергей', 'Анна', 'Иван', 'Наталья',
-  'Михаил', 'Виктория', 'Андрей', 'Юлия', 'Павел'
-];
-
-// Функция для получения случайного элемента массива
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-// Создание одного комментария
-const createComment = () => ({
-  id: createCommentId(),
-  avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(MESSAGES),
-  name: getRandomArrayElement(NAMES)
-});
-
-// Создание массива комментариев
-const createComments = () => {
-  const commentsCount = getRandomInteger(0, 30);
-  const comments = [];
-  for (let i = 0; i < commentsCount; i++) {
-    comments.push(createComment());
+  for (let i = cleanString.length - 1; i >= 0; i--) {
+    reversedString += cleanString[i];
   }
-  return comments;
+
+  return cleanString === reversedString;
 };
 
-// Создание объекта фотографии
-const createPhoto = (id) => ({
-  id,
-  url: `photos/${id}.jpg`,
-  description: `Описание фотографии ${id}`,
-  likes: getRandomInteger(15, 200),
-  comments: createComments()
-});
+const extractNumbers = (input) => {
+  const text = input.toString();
+  let numbers = '';
 
-// Генерация массива из 25 фотографий
-const generatePhotos = () => {
-  const photosArray = [];
-  for (let i = 1; i <= 25; i++) {
-    photosArray.push(createPhoto(i));
+  for (let i = 0; i < text.length; i++) {
+    const character = text[i];
+    if (character >= '0' && character <= '9') {
+      numbers += character;
+    }
   }
-  return photosArray;
-};
 
-// Создаем массив фотографий
-const photos = generatePhotos();
+  if (numbers === '') {
+    return NaN;
+  } else {
+    return parseInt(numbers, 10);
+  }
+};
